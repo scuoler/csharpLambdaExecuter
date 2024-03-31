a7f914e35f48c7705e395393af1427e76638eccb
mathew joseph <mathew_joseph31@yahoo.com>
2024-03-31 18:42:32 -0500

    source file added

---
+++ Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Emit;
+
+namespace ExecuteScript
+{
+
+    class Program
+    {
+        /* How to Execute: 
+        > dotnet run <csharp program>
+
+        Example
+        > dotnet run "
+        using System;
+
+        namespace RoslynCompileSample
+        {
+            public class Writer
+            {
+                public static void Main(string[] message)
+                {
+                    Console.WriteLine($\"Shout it out {message[0]}! \");
+                }
+            }
+        }
+        "
+        */
+
+
+        static void Main(string[] args)
+        {
+            /* check if argument number is valid */
+            if (args.Length <= 0)
+            {
+                throw new Exception("No Argument Supplied, Format: dotnet run <csharp program>");
+            }
+            string code = args[0];
+
+            /*
+                string code=@"
+                using System;
+
+                namespace SomeNameSpace
+                {
+                    public class SomeClass
+                    {
+                        public static void Main(string[] args)
+                        {
+                            Console.WriteLine($"" Shout it out {args[0]} "");
+                        }
+                    }
+                }";
+                */
+            try
+            {
+                /* Parse the code into a CSharpSyntaxTree */
+                SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
+                string assemblyName = Path.GetRandomFileName();
+                var refPaths = new[] {
+                    typeof(System.Object).GetTypeInfo().Assembly.Location,
+                    typeof(Console).GetTypeInfo().Assembly.Location,
+                    Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Runtime.dll")
+                };
+                MetadataReference[] references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
+
+                /* Create a CSharp compilation object from the CSharpSyntaxTree */
+                CSharpCompilation compilation = CSharpCompilation.Create(
+                    assemblyName,
+                    syntaxTrees: new[] { syntaxTree },
+                    references: references,
+                    options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+                /* Create a Semantic Model Object from the CSharpSyntaxTree */
+                SemanticModel semanticModel = compilation.GetSemanticModel(syntaxTree);
+
+                int index = 0;
+                SyntaxNode mainMethodNode = null, mainClassNode = null;
+                ISymbol mainClassSymbol = null;
+                bool mainMethodFound = false;
+                /*
+                  iteration over subtrees of the syntax tree that are of type method declarations
+                */
+                foreach (var i in syntaxTree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>())
+                {
+                    index++;
+                    //Console.WriteLine($"{index} {i.Identifier} {i.ToString()} \n");
+                    /* Look for Main method, find its parent node*/
+                    if (i.Identifier.ToString() == "Main")
+                    {
+                        mainMethodNode = i;
+                        mainMethodFound = true;
+                        mainClassNode = mainMethodNode.Parent;
+                        mainClassSymbol = semanticModel.GetDeclaredSymbol(mainClassNode) as ITypeSymbol;
+                    }
+                }
+                if (!mainMethodFound || (mainClassSymbol == null))
+                {
+                    throw new Exception("A class with main method could not be found");
+                }
+
+                //mainClassNode=syntaxTree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().Last();
+
+                /* Retrieve name of the class that encloses Main methods*/
+                string mainClassName = mainClassSymbol.ToString();
+                //Console.WriteLine(mainClassName);
+                using (var ms = new MemoryStream())
+                {
+                    EmitResult result = compilation.Emit(ms);
+                    if (!result.Success)
+                    {
+                        string errString = "Compilation failed!";
+                        IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                            diagnostic.IsWarningAsError ||
+                            diagnostic.Severity == DiagnosticSeverity.Error);
+
+                        foreach (Diagnostic diagnostic in failures)
+                        {
+                            errString += $" {diagnostic.Id}: {diagnostic.GetMessage()}";
+                        }
+                        throw new Exception(errString);
+                    }
+                    else
+                    {
+                        /* Invoke the main method */
+                        ms.Seek(0, SeekOrigin.Begin);
+                        Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+                        var type = assembly.GetType(mainClassName);
+                        var instance = assembly.CreateInstance(mainClassName);
+                        var meth = type?.GetMember("Main").First() as MethodInfo;
+                        //meth.Invoke(instance, new [] { new []{"mathew"}});
+                        meth?.Invoke(instance, new[] { new[] { " Param " } });
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+    }
+}
+
+
+// See https://aka.ms/new-console-template for more information
---
+++ README.md
@@ -0,0 +1,68 @@
+# Scuoler CSharp Script Executor
+
+[![License: MIT](https://img.shields.io/github/license/mathewjoseph31/schools?style=plastic)](https://opensource.org/licenses/MIT)
+
+## Description
+
+Scuoler (https://scuoler.com/) is a Learning/Content Management System (LMS). This dotnet core program can be used to dynamically compile and run
+a CSharp script string passed as an argument to this program.
+
+The parent repository (https://github.com/MathewJoseph31/scuoler) contains the code for the original Scuoler App (https://scuoler.com/).
+
+Philsophy
+
+At Scuoler, we are thoroughly passionate about innovative ideas and their implementations. We are currently engrossed in the development and promotion of our LMS product, Scuoler. We are committed to spread positive vibes and cutting edge constributions in advancements on software technology and science. If you like our team or philosophy, do contact us by writing to us at (https://scuoler.com/contactUs), and we ensure numerous opportunities towards fruitful collaborations.
+
+## Table of Contents
+
+- [Technologies Utilized](#Technologies)
+- [Screenshot](#Screen)
+- [Deployed Application](#Deployed)
+- [License](#MIT)
+- [Contact](#Contact)
+
+## Technologies Utilized
+
+- [Microsoft Dotnet Core 8.0](https://learn.microsoft.com/en-us/dotnet/core/whats-new/dotnet-8/runtime)
+- [Nuget Package Microsoft.CodeAnalysis.CSharp](https://www.nuget.org/packages/Microsoft.CodeAnalysis.CSharp/)
+- [Posgresql](https://www.postgresql.org/)
+- [Heroku](https://www.heroku.com)
+- [NPM Stripe Package](https://www.npmjs.com/package/stripe)
+- [Node.js](https://nodejs.org/en/)
+- [NPM nodemon Package](https://www.npmjs.com/package/nodemon)
+
+## Screenshot
+
+![Scuoler Logo](./img/scuoler_logo.svg)
+![Scuoler Homepage](./img/scuoler.png)
+
+## Deployed Application
+
+https://scuoler.com/
+
+## MIT License
+
+&copy;2024 Mathew Joseph
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Contact
+
+For inquiries, please contact [Mathew Joseph](https://scuoler.com/contactUs)
+or email me [Mathew Joseph](mailto:mathew@scuoler.com).
